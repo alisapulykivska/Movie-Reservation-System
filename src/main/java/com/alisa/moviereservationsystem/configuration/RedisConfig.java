444ef78f6fd3f649000d83a3807ae1a47bbf50e1
Alisa <alisapulykivska@gmail.com>
2026-04-17 20:24:15 +0200

    added redis configuration

---
+++ src/main/java/com/alisa/moviereservationsystem/configuration/RedisConfig.java
@@ -0,0 +1,42 @@
+package com.alisa.moviereservationsystem.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.JacksonJsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+import java.time.Duration;
+
+@Configuration
+public class RedisConfig {
+    @Bean
+    public RedisConnectionFactory rddConnectionFactory() {
+        return new LettuceConnectionFactory();
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        return template;
+    }
+
+    @Bean
+    public RedisCacheManager redisCacheManager(RedisConnectionFactory redisConnectionFactory) {
+
+        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(10))
+                .disableCachingNullValues()
+                .serializeValuesWith(RedisSerializationContext.SerializationPair
+                        .fromSerializer(new JacksonJsonRedisSerializer<>(Object.class)));
+
+        return RedisCacheManager.builder(redisConnectionFactory)
+                .cacheDefaults(redisCacheConfiguration)
+                .build();
+    }
+}
--- src/main/java/com/alisa/moviereservationsystem/services/ShowtimeService.java
+++ src/main/java/com/alisa/moviereservationsystem/services/ShowtimeService.java
@@ -5,13 +5,14 @@ import com.alisa.moviereservationsystem.dto.returnDto.ShowtimeReturnDto;
 import com.alisa.moviereservationsystem.dto.updateDto.ShowtimeUpdateDto;
 import com.alisa.moviereservationsystem.exceptions.BookedHallException;
 import com.alisa.moviereservationsystem.exceptions.InformationNotFoundException;
-import com.alisa.moviereservationsystem.exceptions.PastShowtimeException;
 import com.alisa.moviereservationsystem.models.Hall;
 import com.alisa.moviereservationsystem.models.Movie;
 import com.alisa.moviereservationsystem.models.Showtime;
 import com.alisa.moviereservationsystem.models.enums.ShowtimeStatus;
 import com.alisa.moviereservationsystem.repositories.*;
 import lombok.AllArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -29,6 +30,7 @@ public class ShowtimeService {
     private final ReservationRepository reservationRepository;
     private final HallRepository hallRepository;
 
+    @CacheEvict(value = "showtimes",  allEntries = true)
     public ShowtimeReturnDto createShowtime(ShowtimeCreateDto showtime) {
         Showtime newShowtime = new Showtime();
 
@@ -59,6 +61,7 @@ public class ShowtimeService {
         return toReturnDto(savedShowtime);
     }
 
+    @CacheEvict(value = "showtimes", allEntries = true)
     public ShowtimeReturnDto updateShowtime(Long showtimeId, ShowtimeUpdateDto showtime) {
         Showtime oldShowtime = showtimeRepository.findById(showtimeId)
                 .orElseThrow(() -> new InformationNotFoundException("Showtime not found"));
@@ -77,16 +80,19 @@ public class ShowtimeService {
         return toReturnDto(savedShowtime);
     }
 
+    @CacheEvict(value = "showtimes", allEntries = true)
     public void deleteShowtime(Long showtimeId) {
         showtimeRepository.deleteById(showtimeId);
     }
 
-    public ShowtimeReturnDto findShowtimeById(Long id) {
-        Showtime showtime = showtimeRepository.findById(id)
+    @Cacheable(value = "showtimes", key = "#showtimeId")
+    public ShowtimeReturnDto findShowtimeById(Long showtimeId) {
+        Showtime showtime = showtimeRepository.findById(showtimeId)
                 .orElseThrow(() -> new InformationNotFoundException("Showtime not found"));
         return toReturnDto(showtime);
     }
 
+    @Cacheable(value = "showtimes")
     public List<ShowtimeReturnDto> findAllShowtimes() {
         return showtimeRepository.findAll()
                 .stream()
@@ -94,6 +100,7 @@ public class ShowtimeService {
                 .toList();
     }
 
+    @Cacheable(value = "showtimes", key = "#movieId")
     public List<ShowtimeReturnDto> getAllShowtimesForMovie(Long movieId) {
         return showtimeRepository.findByMovie_Id(movieId)
                 .stream()
@@ -101,6 +108,7 @@ public class ShowtimeService {
                 .toList();
     }
 
+    @Cacheable(value = "showtimes", key = "#date")
     public List<ShowtimeReturnDto> getShowtimeForDate(LocalDate date) {
         OffsetDateTime start = date.atStartOfDay(ZoneOffset.UTC).toOffsetDateTime();
         OffsetDateTime end = date.plusDays(1).atStartOfDay(ZoneOffset.UTC).toOffsetDateTime();
